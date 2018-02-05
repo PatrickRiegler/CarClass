5cda3dd834901f8bbbfea709a38035ce92c6cdeb
Patrick Riegler (TKR6Q) <Patrick.Riegler@six-group.com>
2018-02-05 17:22:50 +0100

    Initial commit

---
+++ src/Engine.java
@@ -0,0 +1,29 @@
+public class Engine {
+    private String engineType;
+    private int hp;
+    private int ccm;
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public void setEngineType(String engineType) {
+        this.engineType = engineType;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getCcm() {
+        return ccm;
+    }
+
+    public void setCcm(int ccm) {
+        this.ccm = ccm;
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+        Car mycar1 = new Car("Audi", "A1", 2016, 150);
+        System.out.println("Car Brand: "+ mycar1.getMake());
+
+        Car mycar2 = new Car("BMW", "1er", 2014, 125);
+        System.out.println("Car Brand: "+ mycar2.getMake());
+
+    }
+}
